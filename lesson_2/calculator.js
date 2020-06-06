ed1a5e7f8a45f40d20c3b0143afbd3305bdf477f
Jade Vargas <jadevargas92@gmail.com>
2020-06-06 20:29:32 +0000

    finished calculator.js file

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -2,38 +2,64 @@
 // Ask the user for a second number.
 // Ask the user for an operation to perfrom.
 // Perform the operation on the two numbers.
-// Print the result to the terminal. 
+// Print the result to the terminal.
 
 const readline = require("readline-sync");
 
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
 
-console.log('Welcome to our calculator!');
-let firstNumber = Number(readline.question("What is the first number?"));
-let secondNumber = Number(readline.question("What is the second number?"));
+function invalidNumber(number) {
+  return Number.isNaN(Number(number)) || number.trimStart() === '';
+}
 
-console.log(`Your numbers are ${firstNumber} and ${secondNumber}`);
+prompt('Welcome to our calculator!');
+prompt('What is your first number?');
+let firstNumber = readline.question();
 
-let operation = readline.question("What operation would you like to do?\n 1) Add 2) Subtract 3) Multiply 4) Divide");
+while (invalidNumber(firstNumber)) {
+  prompt('Hmm... that doesnt look like a number');
+  firstNumber = readline.question('Try again with a number!');
+}
+
+prompt('What is your second number?');
+let secondNumber = readline.question();
+
+while (invalidNumber(secondNumber)) {
+  prompt('Hmm... that doesnt look like a number');
+  secondNumber = readline.question('Try again with a number!');
+}
+
+prompt(`Your numbers are ${firstNumber} and ${secondNumber}`);
+prompt("What operation would you like to do?\n 1) Add 2) Subtract 3) Multiply 4) Divide");
+let operation = readline.question();
+
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt('Must choose 1, 2, 3 or 4');
+  operation = readline.question();
+}
+
 let outputVersion1;
 let outputVersion2;
 
 switch (operation) {
   case '1':
-    outputVersion1 = firstNumber + secondNumber;
+    outputVersion1 = Number(firstNumber) + Number(secondNumber);
     break;
   case '2':
-    outputVersion1 = firstNumber - secondNumber;
+    outputVersion1 = Number(firstNumber) - Number(secondNumber);
     break;
   case '3':
-    outputVersion1 = firstNumber * secondNumber;
+    outputVersion1 = Number(firstNumber) * Number(secondNumber);
     break;
   case '4':
-    outputVersion1 = firstNumber / secondNumber;
-  default:
-    console.log('Sorry!!')
+    outputVersion1 = Number(firstNumber) / Number(secondNumber);
+    break;
+  default: console.log('Sorry!!');
 }
 
-console.log(`Your output is ${outputVersion1}`);
+prompt(`Your output is ${outputVersion1}`);
 
 
 if (operation === '1') {
@@ -46,4 +72,4 @@ if (operation === '1') {
   outputVersion2 = Number(firstNumber / secondNumber);
 }
 
-console.log(`Your second output is of the course same! It is ${outputVersion2}`);
+prompt(`Your second output is of the course same! It is ${outputVersion2}`);
